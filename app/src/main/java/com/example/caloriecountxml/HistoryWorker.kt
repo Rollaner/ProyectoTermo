b296d74bc6a62201ef6b6e89d5e983adf0d03d8b
Rollaner <sebastianattoni@gmail.com>
2021-12-10 09:37:57 -0300

    history worker carga datos, para prevenir inconsistencias cuando se reinicia la app

--- app/src/main/java/com/example/caloriecountxml/HistoryWorker.kt
+++ app/src/main/java/com/example/caloriecountxml/HistoryWorker.kt
@@ -18,24 +18,26 @@ class HistoryWorker (context: Context, workerParams: WorkerParameters) : Worker(
         val calories = inputData.getString("CALORIES")?:return Result.failure()
         val tdee = inputData.getString("TDEE")?:return Result.failure()
         val date = inputData.getString("DATE")?:return Result.failure()
-        val fileContents = ArrayList<String>(1)
-        fileContents.add("$date,$tdee,$calories,\n")
+        var fileArray = ArrayList<String>(1)
         val file = File(appContext.filesDir, filename)
+
+        if(!file.exists()) file.createNewFile()
+        val fileData = ArrayList(file.readLines())
+
+        if(fileData.isNotEmpty()) fileArray = fileData
+        fileArray.add("$date,$tdee,$calories,\n")
         //Guardar datos en shared preferences o bien shared storage
-        if(fileContents.size == 1){ //si es nuevo
-            file.writeText(fileContents[0])
-            return Result.success()
-        }
-        if(fileContents.size <= 30) { //si es menor o igual a 30
+        if(fileArray.size <= 30) { //si es menor o igual a 30
+            file.writeText(fileArray[0])
             for(i in 1..30) {
-                file.appendText(fileContents[i])
+                file.appendText(fileArray[i])
             }
         }else{
-            fileContents.removeAt(0) //lo demas
-            val aux = fileContents[0]
+            fileArray.removeAt(0) //lo demas
+            val aux = fileArray[0]
             file.writeText(aux)
             for(i in 1..30) {
-                file.appendText(fileContents[i])
+                file.appendText(fileArray[i])
             }
         }
         return Result.success()
